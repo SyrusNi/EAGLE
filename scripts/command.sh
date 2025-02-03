67351cffdd16521d689065548d6fafa985234f3a
SyrusNi <nishengding@126.com>
2025-02-03 23:14:41 +0800

    simplify commands

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,13 +1,10 @@
 srun --pty -N 1 -n 1 -p a800 -q normal --gres=gpu:1 --mem 40GB -t 4:00:00 /bin/bash
 
-accelerate launch -m --num_processes=4 --mixed_precision=bf16 eagle.train.main \
+accelerate launch -m --mixed_precision=bf16 eagle.train.main \
     --basepath models/vicuna-7b-v1.3 \
     --tmpdir data/eagle-generated-data/sharegpt_0_67999_mufp16 \
-    --cpdir test_eagle_vicuna-7b-v1.3 \
-    --configpath eagle/train/vicuna_7B_config.json \
-    --bs 8 \
-    --gradient-accumulation-steps 4 \
-    --ckptdir test_eagle_vicuna-7b-v1.3/state_1
+    --cpdir test_2_eagle_vicuna-7b-v1.3 \
+    --configpath eagle/train/vicuna_7B_config.json
 
 python -m eagle.ge_data.allocation --outdir data/eagle-generated-data
 
@@ -15,26 +12,11 @@ python -m eagle.ge_data.allocation --outdir data/eagle-generated-data
 python -m eagle.evaluation.gen_ea_answer_vicuna \
     --model-id eagle_vicuna-7b-v1.3 \
     --ea-model-path models/EAGLE-Vicuna-7B-v1.3 \
-    --base-model-path models/vicuna-7b-v1.3
-
-python -m eagle.evaluation.gen_ea_answer_vicuna \
-    --model-id eagle_vicuna-7b-v1.3 \
-    --ea-model-path models/EAGLE-Vicuna-7B-v1.3 \
     --base-model-path models/vicuna-7b-v1.3 \
     --temperature 0
 
 python -m eagle.evaluation.gen_baseline_answer_vicuna \
     --model-id baseline_vicuna-7b-v1.3 \
     --ea-model-path models/EAGLE-Vicuna-7B-v1.3 \
-    --base-model-path models/vicuna-7b-v1.3\
-
-python -m eagle.evaluation.gen_baseline_answer_vicuna \
-    --model-id baseline_vicuna-7b-v1.3 \
-    --ea-model-path models/EAGLE-Vicuna-7B-v1.3 \
     --base-model-path models/vicuna-7b-v1.3 \
     --temperature 0
-
-python -m eagle.evaluation.gen_ea_answer_vicuna \
-    --model-id eagle_train_vicuna-7b-v1.3 \
-    --ea-model-path test_eagle_vicuna-7b-v1.3 \
-    --base-model-path models/vicuna-7b-v1.3
